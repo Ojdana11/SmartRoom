169dfb1c81fced07cc915ebebf0a74cc0ca3bc75
Ojdana <aleksandra.ojdana@gmail.com>
2018-10-26 19:02:51 +0200

    Update models.py

--- src/dashboard/models.py
+++ src/dashboard/models.py
@@ -3,25 +3,27 @@ from django.db import models
 
 class Device(models.Model):
     device_id = models.AutoField(primary_key=True)
-    device_name = models.CharField(max_length=30)
-    status = models.CharField(max_length=20)
-    description = models.CharField(max_length=50)
+    device_name = models.CharField(max_length=100)
+    status = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    is_connected = models.BooleanField
 
-    def show_endpoints(self):
+    def get_endpoints(self):
         return Endpoint.objects.filter(device_id=self.device_id)
 
 
 class Endpoint(models.Model):
     endpoint_id = models.AutoField(primary_key=True)
     device_id = models.ForeignKey(Device, on_delete=models.CASCADE)
-    title = models.CharField(max_length=30)
-    card_type = models.CharField(max_length=30)
-    units = models.CharField(max_length=30)
+    endpoint_name = models.CharField(max_length=100)
+    title = models.CharField(max_length=255)
+    card_type = models.CharField(max_length=255)
+    units = models.CharField(max_length=255)
 
-    def show_values(self):
+    def get_values(self):
         return EndpointsValue.objects.filter(endpoint_id=self.id)
 
 
 class EndpointsValue(models.Model):
     endpoint_id = models.ForeignKey(Endpoint, on_delete=models.CASCADE)
-    value = models.CharField(max_length=20)
+    value = models.CharField(max_length=255)
